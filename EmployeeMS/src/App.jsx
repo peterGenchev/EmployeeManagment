660cf01c90674627f466927051c632ed25721106
Nostyone <peter.genchev@yahoo.bg>
2024-01-13 17:20:45 +0200

    add more components

--- EmployeeMS/src/App.jsx
+++ EmployeeMS/src/App.jsx
@@ -1,23 +1,27 @@
-
-import './App.css'
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Dashboard  from './components/Dashboard'
-import Login from './components/Login'
-import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Category from "./components/Category";
+import Profile from "./components/Profile";
+import Employee from "./components/Employee";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/adminlogin' element={<Login />}></Route>
-      <Route path='/dashboard' element={<Dashboard />}>
-      <Route path='' element={<Home />}></Route>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/adminlogin" element={<Login />} />
+        <Route path="/dashboard/*" element={<Dashboard />}>
+          <Route index element={<Home />} />
+          <Route path="employee" element={<Employee />} />
+          <Route path="category" element={<Category />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
